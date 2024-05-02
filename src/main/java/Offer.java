1ad5f2959f0e1ab5d412c04fa33ad9440d1d0647
janaa-wael <janawael119@gmail.com>
2024-05-03 01:06:27 +0300

    Adding Offer Interface

---
+++ src/main/java/Offer.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Jana Wael
+ */
+import java.util.Date;
+
+public interface Offer {
+    public void displayDescription(); //displays or prints description of the offer
+
+    public double  getPriceAfterDiscount(double price , double percentage); //multiplies the price with the 
+
+    public boolean isOfferAvailable(); //checks whether the offer is available for the product or not
+
+    public double getOfferPercentage(); //returns the percentage of the offer 
+    
+    Date getStartDate();
+
+    Date getEndDate();
+}
