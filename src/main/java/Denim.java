91d415d1e5b6c7ed2e20e26d6bd93fd37886852f
Doaa-Said <168484790+Doaa-Said@users.noreply.github.com>
2024-05-06 07:36:38 +0300

    Update Denim.java

--- src/main/java/Denim.java
+++ src/main/java/Denim.java
@@ -5,8 +5,62 @@
 
 /**
  *
- * @author hp
+ * @author Doaa Said
  */
-public class Denim {
+public abstract class  Denim extends Products {
+    private Size size;
+    private String color;
+    private String brand;
+
+
+   
+      public Denim( double price,int id ,String color,String brand){
+      super(price,id);
+      this.color=color;
+      this.brand=brand;
+      
+      }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
     
-}
+    public Size getSize() {
+        return size;
+    }
+      
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+  
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+ @Override
+  public double getOfferPercentage(){
+    if(this.getPrice()<1500&&this.getPrice()>1000)  
+      return 0.3;
+    else if(this.getPrice()<1000&&this.getPrice()>600)
+      return 0.2;
+    else
+        return 0;
+  }
+
+    @Override
+    public String toString() {
+        return super.toString() + "color=" + color +"\n" +" brand=" + brand +"\n";
+    }
+   
+     }
+    
