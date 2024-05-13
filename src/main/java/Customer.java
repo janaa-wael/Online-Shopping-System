e9a9e38ba3c2a78f84369a56e8d240ae2b352cb0
rodMo7A <168588181+rodMo7A@users.noreply.github.com>
2024-05-13 19:12:58 +0300

    Update Customer.java

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,65 +1,76 @@
-
-
 import java.util.ArrayList;
 
 public class Customer extends User {
-	ArrayList<Products> cart = new ArrayList<Products>();
-	ArrayList<Order> myOrders = new ArrayList<Order>();
-	boolean OrderConfirmed;
-	int numberOfOrders;
-	public Customer(){
-		username="";
-		password="";
-	}
-	public Customer(String username,String password) {
-		this.username=username;
-		this.password=password;
-	};
-	
+    ArrayList<Products> cart = new ArrayList<Products>();
+    ArrayList<Order> myOrders = new ArrayList<Order>();
+    boolean OrderConfirmed;
+    int numberOfOrders;
 
-	public int getNumberOfOrders() {
-		return numberOfOrders;
-	}
+    public Customer() {
+        username = "";
+        password = "";
+    }
+//in case a new customer was created by admin
+    public Customer(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-	public void setOrderConfirmed(boolean orderConfirmed) {
-		OrderConfirmed = orderConfirmed;
-	}
+    public int getNumberOfOrders() {
+        return numberOfOrders;
+    }
+//to add order and clear cart
+    public void setOrderConfirmed(boolean orderConfirmed) {
+        OrderConfirmed = orderConfirmed;
+    }
 
-	public void addToCart(Products product , int quantity,Products.Size size) {
-		if(quantity>product.getOneQuantity(size)) {System.out.println("Quantity available = "+product.getOneQuantity(size));}
-		else{product.setOneQuantity(size,product.getOneQuantity(size)-quantity );
-		while(quantity>0) {
-		cart.add(product);
-		quantity--;
-		}
-		
-		}
-	}
-	
-	public void removeFromCart(Products product,int quantity,Products.Size size) {
-		try{product.setOneQuantity(size,product.getOneQuantity(size)+quantity );
-		while(quantity>0) {
-			cart.remove(product);
-			quantity--;
-			}
-		
-		}
-		catch(Exception ex) {}
-	}
-	
-	public ArrayList<Products> getCart() {
-		return cart;
-	}
-	
-	public void addOrder(Order order) {
-		
-		if(OrderConfirmed) {
-			myOrders.add(order);
-			cart.clear();
-			this.numberOfOrders++;
-		}
-		else System.out.println("Please confirm your order");
-		
-	}
-	
+    public void addToCart(Products product, int quantity, Products.Size size) {
+        try {
+            int availableQuantity = product.getOneQuantity(size);
+            if (quantity > availableQuantity) {
+                System.out.println("Not enough stock. Quantity available = " + availableQuantity);
+            } else {
+                product.setOneQuantity(size, availableQuantity - quantity);
+                for (int i = 0; i < quantity; i++) {
+                    cart.add(product);
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println("Failed to add product to cart: " + ex.getMessage());
+        }
+    }
+
+    public void removeFromCart(Products product, int quantity, Products.Size size) {
+        try {
+            if (!cart.contains(product)) {
+                throw new IllegalArgumentException("Product not found in cart.");
+            }
+            int toRemove = Math.min(quantity, cart.size());
+            for (int i = 0; i < toRemove; i++) {
+                cart.remove(product);
+            }
+            product.setOneQuantity(size, product.getOneQuantity(size) + toRemove);
+        } catch (Exception ex) {
+            System.err.println("Failed to remove product from cart: " + ex.getMessage());
+        }
+    }
+
+    public ArrayList<Products> getCart() {
+        return cart;
+    }
+
+    public void addOrder(Order order) {
+        try {
+            if (!OrderConfirmed) {
+                throw new IllegalStateException("Please confirm your order before adding.");
+            }
+            myOrders.add(order);
+            cart.clear();
+            numberOfOrders++;
+        } catch (IllegalStateException ex) {
+            System.err.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.err.println("An error occurred while adding the order: " + ex.getMessage());
+        }
+    }
 }
