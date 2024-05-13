9179d0cccc48f820a90ce33abdd87eefd69b87fd
janaa-wael <janawael119@gmail.com>
2024-05-13 16:56:32 +0300

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList; 
 public abstract class User {
-        String username;
-	//String email;
-	String password;
-	ArrayList<String> customers = new ArrayList<String>();
-	ArrayList<Products> products = new ArrayList<String>();
+        protected String username;
+	protected String password;
+	protected ArrayList<String> customers = new ArrayList<String>();
+	protected ArrayList<Products> products = new ArrayList<String>();
     public boolean isProductAvailable(String productID) {
 		if (products.contains(productID)) return true;
 		else return false;
